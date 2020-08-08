18454e994cc3d57cfd09de0db7450817acc13927
Amit Jaydeepsingh Thakur <57566758+projectelex@users.noreply.github.com>
2020-08-08 11:57:54 +0530

    Create sum of left leaves.java

---
+++ DSA/Trees/sum of left leaves.java
@@ -0,0 +1,35 @@
+Link-https://leetcode.com/interview/1/submissions/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+      return(helper(root,false));
+    }
+  
+  public int helper(TreeNode root,boolean isLeft)
+  {
+    if(root==null)
+    {
+      return(0);
+    }
+    if(root.left==null && root.right==null && isLeft==true)
+    {
+      return(root.val);
+    }
+    return(helper(root.left,true)+helper(root.right,false));
+  }
+}
